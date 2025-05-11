de6c74abc27c4245676b074fed4f154c3d1d55ec
VikasMeshram2708 <meshramvikas744@gmail.com>
2025-05-11 12:46:29 +0530

    :tada: added: Navbar and updated hero section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
     <div className="realtive min-h-screen w-full">
-      <div className="absolute inset-x-0 from-purple-500/10 to-transparent"></div>
-      <div className="flex flex-col items-center justify-center">
-        <h2>What is your name</h2>
-        <p>My Name is Vikas.</p>
+      <div className="absolute inset-x-0 bg-gradient-to-b from-purple-500/10 h-96 to-transparent"></div>
+      <div className="flex flex-col items-center justify-center h-96">
+        <h2 className="text-lg sm:text-2xl md:text-3xl font-bold font-serif">
+          What is your name
+        </h2>
+        <p className="text-base sm:text-lg">My Name is Vikas.</p>
       </div>
     </div>
   );
